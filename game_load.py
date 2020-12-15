d22a9502cd546be417f32fe81995bfafd7c9661b
frankTheCodeBoy <olumfrank48@gmail.com>
2020-12-15 16:35:34 +0300

    Phase 2 Game Project.

--- game_load.py
+++ game_load.py
@@ -15,13 +15,31 @@ class GameCharacter:
     def run_the_game(self):
         """a while loop for the game"""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.bg_color)
-            self.goku.blitme()
+            self._check_the_event()
+            self.goku.update_me()
+            self._update_the_screen()
 
-            pygame.display.flip()
+    def _check_the_event(self):
+        """watch for keyboard and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.goku.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.goku.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.goku.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.goku.moving_left = False
+
+    def _update_the_screen(self):
+        """fill screen color, blit new image at point"""
+        self.screen.fill(self.bg_color)
+        self.goku.blitme()
+        pygame.display.flip()
 
 if __name__ == "__main__":
     version_one = GameCharacter()
--- goku.py
+++ goku.py
@@ -15,6 +15,16 @@ class Goku:
         # positioning
         self.rect.center = self.screen_rect.center
 
+        # give image ability to move by first setting flag
+        self.moving_right = False
+        self.moving_left = False 
+
     def blitme(self):
         """draw character at said position"""
         self.screen.blit(self.scaled_image,self.rect)
+
+    def update_me(self):
+        if self.moving_right==True:
+            self.rect.x += 1
+        if self.moving_left==True:
+            self.rect.x -= 1
