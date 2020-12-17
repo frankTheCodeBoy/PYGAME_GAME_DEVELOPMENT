d923f5a42a43dd9b2747edd39e637970b4fc609b
frankTheCodeBoy <olumfrank48@gmail.com>
2020-12-17 20:55:20 +0300

    Phase III(b) Game Project

---
+++ game_bullets.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """A class to manage character bullets"""
+    def __init__(self,first_play):
+        """Create bullets at character location"""
+        super().__init__()
+        self.screen = first_play.screen
+        # bullet settings
+        self.bullet_height = 15
+        self.bullet_width = 3
+        self.bullet_color = (250,0,0)
+        self.bullet_speed = 1.0
+        # Create bullet at 0,0 then correct position
+        self.rect = pygame.Rect(0,0,self.bullet_width,self.bullet_height)
+        self.rect.center = first_play.goku.rect.center
+        # Store bullet speed as float value
+        self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the bullet horizontally across the screen"""
+        self.x += self.bullet_speed
+        self.rect.x = self.x
+
+    def draw_bullet(self):
+        """Draw bullet on the screen"""
+        pygame.draw.rect(self.screen,self.bullet_color,self.rect)
--- game_load.py
+++ game_load.py
@@ -1,22 +1,28 @@
 import sys
 import pygame
 from goku import Goku
+from game_bullets import Bullet
+
 class GameCharacter:
     """a class to manage game resources and activities"""
     def __init__(self):
         """initialize game and define attributes"""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1150,700))
+        
+        self.screen_width = 700
+        self.screen_height = 1150
+        self.screen = pygame.display.set_mode((self.screen_height,self.screen_width))
         self.bg_color = (100,100,100)
         pygame.display.set_caption("Game Character- Version One: created by @frank olum.")
         self.goku = Goku(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_the_game(self):
         """a while loop for the game"""
         while True:
             self._check_the_event()
             self.goku.update_me()
+            self._update_bullets()
             self._update_the_screen()
 
     def _check_the_event(self):
@@ -33,6 +39,9 @@ class GameCharacter:
                     self.goku.moving_up = True
                 elif event.key == pygame.K_DOWN:
                     self.goku.moving_down = True
+                elif event.key == pygame.K_SPACE:
+                    new_bullet = Bullet(self)
+                    self.bullets.add(new_bullet)
                 elif event.key == pygame.K_q:
                     sys.exit()
             elif event.type == pygame.KEYUP:
@@ -40,15 +49,21 @@ class GameCharacter:
                     self.goku.moving_right = False
                 elif event.key == pygame.K_LEFT:
                     self.goku.moving_left = False
-                elif event.key == pygame.K_UP:
+                elif event.key  == pygame.K_UP:
                     self.goku.moving_up = False
                 elif event.key == pygame.K_DOWN:
                     self.goku.moving_down = False
+
+    def _update_bullets(self):
+        """Update positions of bullets and rid old bullets"""
+        self.bullets.update()
                
     def _update_the_screen(self):
         """fill screen color, blit new image at point"""
         self.screen.fill(self.bg_color)
         self.goku.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 if __name__ == "__main__":
