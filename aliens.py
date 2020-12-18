5a11eae69679b28e71065a7e8bd45f409dad43f5
frankTheCodeBoy <olumfrank48@gmail.com>
2020-12-18 21:46:35 +0300

    alien file added to project

---
+++ aliens.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.sprite import Sprite 
+
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet"""
+
+    def __init__(self,first_play):
+        """Initialize alien and set its starting position"""
+        super().__init__()
+        self.screen = first_play.screen
+
+        # Load image and set rect attribute
+        self.image = pygame.image.load("game_pics/space_ship.png")
+        self.rect = self.image.get_rect()
+
+        # Start each image near the top left screen corner
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the aliens exact horizontal position
+        self.x = float(self.rect.x)
+
+
