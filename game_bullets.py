66b0decdea54479dcffe18d7cc9922a6629b42d0
frankTheCodeBoy <olumfrank48@gmail.com>
2020-12-18 11:12:53 +0300

    Bullet Modified in py

--- game_bullets.py
+++ game_bullets.py
@@ -10,7 +10,7 @@ class Bullet(Sprite):
         # bullet settings
         self.bullet_height = 15
         self.bullet_width = 3
-        self.bullet_color = (60,60,60)
+        self.bullet_color = (200,200,200)
         self.bullet_speed = 1.0
         
         # Create bullet at 0,0 then correct position
