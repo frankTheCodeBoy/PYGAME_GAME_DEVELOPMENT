e4cfa1d09f96483563963ae3e69cc996633e93a4
frankTheCodeBoy <olumfrank48@gmail.com>
2020-12-25 19:10:31 +0300

    Phase 9

---
+++ settings.py
@@ -0,0 +1,8 @@
+class FleetSettings:
+    """A class to manage fleet"""
+    def __init__(self):
+        self.alien_speed = 1.0
+        self.fleet_cruise_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+        
